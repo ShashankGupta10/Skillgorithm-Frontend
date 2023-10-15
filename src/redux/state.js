6c2648ca2891718aa5687746bb93814560735f9b
Shashank <shashankgupta9248@gmail.com>
2023-10-15 17:24:16 +0530

    redux

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,7 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  mode: "light",
   user: null,
   token: null,
 };
@@ -10,10 +9,6 @@ export const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    // setMode: (state) => {
-    //   state.mode = state.mode === "light" ? "dark" : "light";
-    // },
-
     setLogin: (state, action) => {
       state.user = action.payload.user;
       state.token = action.payload.token;
