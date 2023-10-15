e6362f27fcc68b4334cfb645d224e4922236e284
Shashank <shashankgupta9248@gmail.com>
2023-10-15 19:36:25 +0530

    homepage figma wala

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
+import Home from "./components/Landing/Home";
 function App() {
   const mode = useSelector((state) => state.mode);
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Hello World</h1>} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
